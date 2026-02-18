e8d2d0ce03ed7937635f9f85c8a8716e9da53f39
Lankala Durga Prasanna Kumar <lankalakumar08@gmail.com>
2026-02-18 22:58:23 +0530

    small updates

--- backend/src/models/Task.js
+++ backend/src/models/Task.js
@@ -25,7 +25,7 @@ const TaskSchema = new mongoose.Schema(
     },
 
     priority: {
-      type: String,
+      type: Number,
       enum: [1,2,3], // 1=low, 2=medium, 3=high
       default: "medium",
     },
